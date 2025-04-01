6a545ddc1bcc428b4c7064016fcf5b1f08ed9ca8
jinhuiliang01 <166369077+jinhuiliang01@users.noreply.github.com>
2025-04-01 14:36:22 +1100

    updates

---
+++ week5/script.js
@@ -0,0 +1,9 @@
+console.log("Hello");
+console.log("How are you my bro");
+
+let myName = "Laaam";
+console.log("Hello", myName);
+myName = "Hins";
+console.log("Hello", myName);
+let no = 10;
+console.log("Hello", no);
